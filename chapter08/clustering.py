9d64776fe926819e3c8b891591011f32cb69fa76
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2019-04-01 12:38:30 +0200

    Implemented Lloyd algorithm for clustering with its unit-tests

--- chapter08/clustering.py
+++ chapter08/clustering.py
@@ -22,3 +22,29 @@ def sq_error_distortion(data_points, centers):
     centers = np.array(centers)
     dist = sum([min([euclidean(point, center) for center in centers]) ** 2 for point in points]) / len(points)
     return dist
+
+
+def lloyd_cluster(data_points, centers):
+    points = np.array(data_points, dtype=float)
+    centers = np.array(centers)
+    k = len(centers)
+
+    # As long as centers have moved in the previous iteration
+    prev_centers = None
+    while prev_centers is None or not np.all(np.isclose(prev_centers, centers, atol=0, rtol=1e-6)):
+        clusters = {center_idx: [] for center_idx in range(0, k)}
+        # Assign each data point to the closest center
+        for point in points:
+            closest_center_idx = np.argmin([euclidean(point, center) for center in centers])
+            current_list = clusters[closest_center_idx]
+            current_list.append(point)
+            clusters[closest_center_idx] = current_list
+            # clusters[closest_center_idx].append(point)
+        # Re-assign each cluster center to the center of gravity for that cluster
+        prev_centers = np.ndarray.copy(centers)
+        for clusters_idx, cluster_points in clusters.items():
+            cluster_points = np.array(cluster_points, dtype=float)
+            center_of_gravity = np.mean(cluster_points, axis=0)
+            centers[clusters_idx] = center_of_gravity
+
+    return centers.tolist()
--- chapter08/test_clustering.py
+++ chapter08/test_clustering.py
@@ -37,3 +37,29 @@ def test_sq_error_distortion():
     data_points, centers, k = fetch_sq_error_dist_input(Path('test/testcase05.txt'))
     dist = clustering.sq_error_distortion(data_points, centers)
     assert np.isclose(dist, 28.69774363476738)
+
+
+def test_lloyd_cluster():
+    k, data_points = fetch_farthest_first_centers_input(Path('test/testcase06.txt'))
+    centers = clustering.lloyd_cluster(data_points=data_points, centers=data_points[:k])
+    assert np.all(np.isclose(centers, [[1.800, 2.867], [1.060, 1.140]], rtol=0, atol=1e-3))
+
+    k, data_points = fetch_farthest_first_centers_input(Path('test/testcase07.txt'))
+    centers = clustering.lloyd_cluster(data_points=data_points, centers=data_points[:k])
+    expected = [[7.561403508771929, 6.1672514619883065, 16.568421052631578, 6.078362573099413, 7.0964912280701755],
+                [18.23220338983051, 6.14745762711864, 5.4677966101694935, 6.577966101694915, 6.0533898305084755],
+                [7.03664596273292, 17.298757763975154, 6.927329192546586, 5.495031055900623, 7.027950310559],
+                [7.7124999999999995, 7.232812500000005, 6.916406250000001, 18.717187499999998, 6.838281249999997],
+                [6.041666666666666, 6.278846153846154, 5.708333333333335, 7.014102564102566, 17.408333333333328],
+                [5.15814814814815, 4.55925925925926, 5.112592592592593, 5.143703703703705, 4.718888888888891]]
+    assert np.all(np.isclose(centers, expected, rtol=0, atol=1e-3))
+
+    k, data_points = fetch_farthest_first_centers_input(Path('test/testcase08.txt'))
+    centers = clustering.lloyd_cluster(data_points=data_points, centers=data_points[:k])
+    expected = [[4.307142857142859, 10.86111111111111, 16.488888888888887],
+                [5.606140350877197, 11.50438596491228, 4.507894736842106],
+                [17.24605809128631, 7.417842323651452, 6.301244813278007],
+                [8.157777777777778, 20.985555555555557, 7.939999999999995],
+                [4.922340425531912, 3.6191489361702125, 4.70345744680851],
+                [8.710439560439553, 3.365384615384615, 14.840659340659354]]
+    assert np.all(np.isclose(centers, expected, rtol=0, atol=1e-3))
